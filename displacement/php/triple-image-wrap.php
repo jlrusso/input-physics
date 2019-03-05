b62b40519fc2b26e66d4096bc7e4c73198489d31
James Russo <1jlrusso@gmail.com>
2019-03-05 15:04:31 -0800

    first commit of isolated triple-image section

---
+++ displacement/php/triple-image-wrap.php
@@ -0,0 +1,29 @@
+<?php 
+
+echo "
+  <div class='panel panel-default' id='triple-image-wrap'>
+    <div class='panel-body'>
+      <div class='row animated fadeInLeft' id='third-pics-row'>
+        <div class='col-sm-4 triple-image'>
+          <img src='../images/displacement/footballField.jpg' alt='Football Field' class='img-responsive'>
+          <div class='img-caption'>
+            <p>NFL player Jim Marshall ran a fumble 66 yards to his own endzone. As in the NFL, direction matters in dislpacement.</p>
+          </div>
+        </div>
+        <div class='col-sm-4 triple-image'>
+          <img src='../images/displacement/curvedRoad.jpg' alt='Track and Field' class='img-responsive'>
+          <div class='img-caption'>
+            <p>If the distance from end to end in this loop is 200 meters, the net displacement might only be 30 meters</p>
+          </div>
+        </div>
+        <div class='col-sm-4 triple-image'>
+          <img src='../images/displacement/raceStart.jpg' alt='Race Start' class='img-responsive'>
+          <div class='img-caption'>
+            <p>800 meter track runners may start at different points along the track, but the total distance ran is always 800 meters.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class='line-divider'></div>
+";
