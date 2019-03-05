b5ae7d118a52ee9b9156f01a76f73fb9c0da71ec
James Russo <1jlrusso@gmail.com>
2019-03-05 15:09:21 -0800

    first commit of isolated practice problem 1 modal

---
+++ displacement/php/example-modal-one.php
@@ -0,0 +1,50 @@
+<?php 
+
+echo "
+  <div class='modal fade example-modal' id='my-modal-one' role='dialog'>
+    <div class='modal-dialog'>
+      <div class='modal-content'>
+        <div class='modal-header'>
+          <h4 class='text-center'>Displacement</h4>
+        </div>
+        <div class='modal-body'>
+          <button class='accordion first-accordion'><strong>Problem</strong></button>
+          <div class='modal-panel'>
+            <p>
+            James is taking a drive to the supermarket 10 miles east of his home to buy some groceries. On his way back home he will stop at a gas station 2.4 miles west of the supermarket. What will his net displacement be during that time period?
+            </p>
+          </div>
+          <button class='accordion'><strong>Given Variables</strong></button>
+          <div class='modal-panel'>
+            <p>
+              <b>Supermarket</b> = 10 miles west<br/>
+              <b>Gas station</b> = 2.4 miles east of the supermarket<br/>
+              <b>Displacement</b> = net distance traveled<br/>
+            </p>
+          </div>
+          <button class='accordion'><strong>Illustration</strong></button>
+          <div class='modal-panel text-center'>
+            <p>
+            <img src='../images/displacement/displacement-accordion-picture-one.PNG' alt='Displacement Illustration' class='img-responsive'>
+            </p>
+          </div>
+          <button class='accordion last-accordion'><strong>Solution</strong></button>
+          <div class='modal-panel'>
+            <p>
+              <ol>
+                <li><b>Positive Displacement</b> = west</li>
+                <li><b>Negative Displacement</b> = east</li>
+                <li><b>First vector</b> = 10 miles west <b>(positive)</b></li>
+                <li><b>Second vector</b> = 2.4 miles east <b>(negative)</b></li>
+                <li><b>Net displacement</b> = 10 - 2.4 = 7.6 miles (west) <b>(positive)</b></li>
+              </ol>
+            </p>
+          </div>
+        </div>
+        <div class='modal-footer text-center'>
+          <button type='button' class='btn glyphicon glyphicon-remove close-ex-btn' data-dismiss='modal'></button>
+        </div>
+      </div>
+    </div>
+  </div>
+";
