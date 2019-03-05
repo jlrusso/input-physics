d219ac85cb17ea7b51e2c25e0b93bd15183f226f
James Russo <1jlrusso@gmail.com>
2019-03-05 15:09:45 -0800

    first commit of practice problem 3 modal

---
+++ displacement/php/example-modal-three.php
@@ -0,0 +1,49 @@
+<?php 
+
+echo "
+  <div class='modal fade example-modal' id='my-modal-three' role='dialog'>
+    <div class='modal-dialog'>
+      <div class='modal-content'>
+        <div class='modal-header'>
+          <h4 class='text-center'>Initial Position</h4>
+        </div>
+        <div class='modal-body'>
+          <button class='accordion first-accordion'><strong>Problem</strong></button>
+          <div class='modal-panel'>
+            <p>
+              Tom is practicing for his 200 meter race by running up a hill. Suppose he ran 150 meters and stopped to take a break. His displacement was  120 meters. What was in starting position in meters?
+            </p>
+          </div>
+          <button class='accordion'><strong>Given Variables</strong></button>
+          <div class='modal-panel'>
+            <p>
+              <ul>
+                <li><b>Equation</b> : X<sub>i</sub> = X<sub>f</sub> - &Delta;x</li>
+                <li><b>Hill Height</b> : 200 meters</li>
+                <li><b>Final Position</b> : 150 meters up hill</li>
+                <li><b>Displacement</b> : 120 meters up hill</li>
+              </ul>
+            </p>
+          </div>
+          <button class='accordion'><strong>Illustration</strong></button>
+          <div class='modal-panel text-center'>
+            <img src='../images/displacement/displacement-accordion-picture-three.PNG' alt='Displacement Illustration' class='img-responsive'>
+          </div>
+          <button class='accordion last-accordion'><strong>Solution</strong></button>
+          <div class='modal-panel'>
+            <p>
+              <ol>
+                <li><b>Equation</b> : X<sub>i</sub> = X<sub>f</sub> - &Delta;x</li>
+                <li><b>Equation With Values</b> : (150 m) - (120 m)</li>
+                <li><b>Initial Position</b> : (150 m) - (120 m) = 30 m</li>
+              </ol>
+            </p>
+          </div>
+        </div>
+        <div class='modal-footer text-center'>
+          <button type='button' class='btn glyphicon glyphicon-remove close-ex-btn' data-dismiss='modal'></button>
+        </div>
+      </div>
+    </div>
+  </div>
+";
