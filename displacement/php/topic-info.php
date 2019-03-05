879f1dc9202440205f660478c54e006ebc259652
James Russo <1jlrusso@gmail.com>
2019-03-05 15:07:31 -0800

    first commit of isolated topic-info section

---
+++ displacement/php/topic-info.php
@@ -0,0 +1,48 @@
+<?php 
+
+echo "
+  <div class='col-md-4' id='topic-info'>
+    <div class='panel panel-default'>
+      <div class='panel-heading'>
+        <div class='panel-title text-center'><h4 id='background-heading'><b>Background:</b></h4></div>
+      </div>
+      <div class='panel-body module-section'>
+        <div class='module-box'>
+          <div class='module'>
+            <div class='module-front' id='first-front'>Displacement</div>
+            <div class='module-back' id='first-back'>
+              Net distance between final position and initial position
+            </div>
+          </div>
+        </div>
+        <div class='module-box'>
+          <div class='module'>
+            <div class='module-front'>Positive Motion</div>
+            <div class='module-back'>
+              Conventionally upwards and / or to the right of the origin
+            </div>
+          </div>
+        </div>
+        <div class='module-box'>
+          <div class='module'>
+            <div class='module-front'>Negative Motion</div>
+            <div class='module-back'>
+              Conventionally downwards and / or to the left of the origin
+            </div>
+          </div>
+        </div>
+        <div class='module-box'>
+          <div class='module bottom-corners-rounded'>
+            <div class='module-front' id='last-front'>Distance</div>
+            <div class='module-back' id='last-back'>
+              Total distance travelled in any direction for given period of time
+            </div>
+          </div>
+        </div>
+        <div class='module-img'>
+          <img src='../images/displacement/positionVTimeGraph.PNG' class='img-responsive' alt='Position v. Time Graph'>
+        </div>
+      </div>
+    </div>
+  </div>
+";
