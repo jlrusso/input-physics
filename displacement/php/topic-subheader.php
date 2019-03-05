07a5c048638d0f280ad6d3a95c2f8ceb43108d39
James Russo <1jlrusso@gmail.com>
2019-03-05 15:06:14 -0800

    first commit of isolated topic subheader section

---
+++ displacement/php/topic-subheader.php
@@ -0,0 +1,14 @@
+<?php 
+
+echo "
+  <div class='row' id='topic-subheader'>
+    <div class='col-sm-12 text-center'>
+      <h2 class='animated bounceInDown section-heading'>Displacement</h2>
+      <div class='pages-nav animated bounceInDown' id='top-next'>
+        <ul class='pager'>
+          <li><a href='../vector-displacement/vector-displacement.php' class='next-page-btn'>Next: Vector Displacement<span class='glyphicon glyphicon-chevron-right'></span></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+";
